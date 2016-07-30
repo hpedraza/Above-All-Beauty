6d447ea982b7a5a4829f297bd289d1c75ac3fcaa
hpedraza <hpedraza2333@gmail.com>
2016-07-29 21:04:37 -0500

    Add project files.

---
+++ Above All Beauty/Scripts/Mine/googleMaps.js
@@ -0,0 +1,18 @@
+﻿function initialize() {
+
+    var mapUpcoming = {
+        center: new google.maps.LatLng(26.1978497, -98.2774268),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById('mapUpcoming'), mapUpcoming);
+
+    var conventionCenterMarker = new google.maps.Marker({
+        position: myCenter
+    });
+
+    conventionCenterMarker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ Above All Beauty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Above_All_Beauty.Startup))]
+namespace Above_All_Beauty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
